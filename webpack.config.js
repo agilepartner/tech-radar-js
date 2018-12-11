fed76617de12cf124df70d6235553a3a9db15adf
Yoan THIRION <ythirion@agilepartner.net>
2018-12-11 16:58:06 +0100

    Generate expander for rings

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,76 @@
-'use strict';
+"use strict";
 
-const webpack = require('webpack');
-const path = require('path');
-const buildPath = path.join(__dirname, './dist');
-const args = require('yargs').argv;
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const buildPath = path.join(__dirname, "./dist");
+const args = require("yargs").argv;
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-let isProd = args.prod;
+let outputPath = "_build";
+
+let main = ["./src/site.js"];
+let common = ["./src/common.js"];
+let devtool;
+let pathsToClean = [outputPath];
 let isDev = args.dev;
 
-let outputPath = '_build';
-
-let main = ['./src/site.js'];
-let common = ['./src/common.js'];
-let devtool;
-let pathsToClean = [
-    outputPath
-];
-
 if (isDev) {
-    main.push('webpack-dev-server/client?http://0.0.0.0:8080');
-    devtool = 'source-map';
+  main.push("webpack-dev-server/client?http://0.0.0.0:8080");
+  devtool = "source-map";
 }
 
 let plugins = [
-    new ExtractTextPlugin('[name].[hash].css'),
-    new HtmlWebpackPlugin({
-        template: './src/index.html',
-        chunks: ['main'],
-        inject: 'body'
-    }),
-    new CleanWebpackPlugin(pathsToClean)
+  new ExtractTextPlugin("[name].[hash].css"),
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    chunks: ["main"],
+    inject: "body",
+    minify: false
+  }),
+  new CleanWebpackPlugin(pathsToClean)
 ];
 
-if (isProd) {
-    plugins.push(
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            },
-            mangle: true
-        }),
-        new webpack.optimize.OccurenceOrderPlugin()
-    );
-}
-
 module.exports = {
-    entry: {
-        'main' : main,
-        'common' : common
-    },
-    node: {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty',
-        child_process: 'empty'
-    },
-    output: {
-        path: outputPath,
-        filename: '[name].[hash].js'
-    },
+  entry: {
+    main: main,
+    common: common
+  },
+  node: {
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+    child_process: "empty"
+  },
+  output: {
+    path: outputPath,
+    filename: "[name].[hash].js"
+  },
 
-    module: {
-        loaders: [
-            { test: /\.json$/, loader: 'json'},
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
-            { test: /\.scss$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap!sass', ) },
-            { test: /\.(png|jpg|ico)$/, exclude: /node_modules/, loader: 'file-loader?name=images/[name].[ext]&context=./src/images' }
-        ]
-    },
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: "json" },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract("style", "css?sourceMap!sass")
+      },
+      {
+        test: /\.(png|jpg|ico)$/,
+        exclude: /node_modules/,
+        loader: "file-loader?name=images/[name].[ext]&context=./src/images"
+      }
+    ]
+  },
 
-    quiet: false,
-    noInfo: false,
+  quiet: false,
+  noInfo: false,
+  plugins: plugins,
+  devtool: devtool,
 
-    plugins: plugins,
-
-    devtool: devtool,
-
-    devServer: {
-        contentBase: buildPath,
-        host: '0.0.0.0',
-        port: 8080
-    }
+  devServer: {
+    contentBase: buildPath,
+    host: "0.0.0.0",
+    port: 8080
+  }
 };
-
